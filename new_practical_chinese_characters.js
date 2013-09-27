f8743c5bbf70568c24b0f7492774d5f5805c07e0
Kai Carver <kaicarver@gmail.com>
2013-09-27 23:04:42 +0800

    initial version of flashcards

---
+++ new_practical_chinese_characters.js
@@ -0,0 +1,662 @@
+// from http://www.skritter.com/vocab/list?list=115280917
+// converted from CSV to JSON via http://www.convertcsv.com/csv-to-json.htm
+var words = [
+  {
+    "char":"一",
+    "pinyin":"yī",
+    "english":"one; once; a (article) (Kangxi radical 1)"
+  },
+  {
+    "char":"二",
+    "pinyin":"èr",
+    "english":"two (Kangxi radical 7)"
+  },
+  {
+    "char":"三",
+    "pinyin":"sān",
+    "english":"three"
+  },
+  {
+    "char":"四",
+    "pinyin":"sì",
+    "english":"four"
+  },
+  {
+    "char":"五",
+    "pinyin":"wǔ",
+    "english":"five"
+  },
+  {
+    "char":"六",
+    "pinyin":"liù",
+    "english":"six"
+  },
+  {
+    "char":"七",
+    "pinyin":"qī",
+    "english":"seven"
+  },
+  {
+    "char":"八",
+    "pinyin":"bā",
+    "english":"eight (Kangxi radical 12)"
+  },
+  {
+    "char":"九",
+    "pinyin":"jiǔ",
+    "english":"nine"
+  },
+  {
+    "char":"十",
+    "pinyin":"shí",
+    "english":"ten (Kangxi radical 24)"
+  },
+  {
+    "char":"我",
+    "pinyin":"wǒ",
+    "english":"I; me"
+  },
+  {
+    "char":"你",
+    "pinyin":"nǐ",
+    "english":"you (singular)"
+  },
+  {
+    "char":"是",
+    "pinyin":"shì",
+    "english":"be; is; are; am"
+  },
+  {
+    "char":"要",
+    "pinyin":"yào, yāo",
+    "english":"to want; to need; will/shall; important\ndemand; request"
+  },
+  {
+    "char":"有",
+    "pinyin":"yǒu",
+    "english":"have; there is"
+  },
+  {
+    "char":"不",
+    "pinyin":"bù",
+    "english":"no; not"
+  },
+  {
+    "char":"很",
+    "pinyin":"hěn",
+    "english":"very; rather; quite; (adverb of degree)"
+  },
+  {
+    "char":"吗",
+    "pinyin":"ma",
+    "english":"(question particle for yes/no questions)",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"嗎",
+    "pinyin":"ma",
+    "english":"(question particle for yes/no questions)",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"的",
+    "pinyin":"de, dì, dí, dī",
+    "english":"(possessive, modifying, or descriptive particle)\nbull's eye\nin reality"
+  },
+  {
+    "char":"在",
+    "pinyin":"zài",
+    "english":"at; on; in; exist; (indicating an action in progress)"
+  },
+  {
+    "char":"上",
+    "pinyin":"shàng",
+    "english":"above; up; on top; get on; go up"
+  },
+  {
+    "char":"这",
+    "pinyin":"zhè, zhèi",
+    "english":"this; these",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"這",
+    "pinyin":"zhè, zhèi",
+    "english":"this; these",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"那",
+    "pinyin":"nà, nèi",
+    "english":"that; in that case; then"
+  },
+  {
+    "char":"里",
+    "pinyin":"lǐ",
+    "english":"inside; internal; village; Chinese mile (~.5 km); neighborhood (Kangxi radical 166)",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"裡",
+    "pinyin":"lǐ",
+    "english":"inside; internal; village",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"请",
+    "pinyin":"qǐng",
+    "english":"please; invite; to request\ntreat",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"請",
+    "pinyin":"qǐng",
+    "english":"please; invite; to request\ntreat",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"问",
+    "pinyin":"wèn",
+    "english":"ask; inquire",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"問",
+    "pinyin":"wèn",
+    "english":"ask; inquire",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"好",
+    "pinyin":"hǎo, hào",
+    "english":"good; well; nice; very; quite\nbe fond of"
+  },
+  {
+    "char":"没",
+    "pinyin":"méi, mò",
+    "english":"not have; there is not; (negative prefix for verbs)\nto drown; submerge",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"沒",
+    "pinyin":"méi, mò",
+    "english":"not have; there is not; (negative prefix for verbs)\nto drown; submerge",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"天",
+    "pinyin":"tiān",
+    "english":"day; sky; heaven; weather"
+  },
+  {
+    "char":"能",
+    "pinyin":"néng",
+    "english":"can; be able; capable; energy; ability"
+  },
+  {
+    "char":"下",
+    "pinyin":"xià",
+    "english":"descend; fall; below; down; next; the second (of two parts)"
+  },
+  {
+    "char":"中",
+    "pinyin":"zhōng, zhòng",
+    "english":"middle; among; China\nto hit (the mark); be hit; receive"
+  },
+  {
+    "char":"可",
+    "pinyin":"kě",
+    "english":"can; may; able to; but; approve; very"
+  },
+  {
+    "char":"以",
+    "pinyin":"yǐ",
+    "english":"to use; according to; so as to; for; by"
+  },
+  {
+    "char":"公",
+    "pinyin":"gōng",
+    "english":"public; fair; honorable (designation); international"
+  },
+  {
+    "char":"车",
+    "pinyin":"chē, jū",
+    "english":"car; vehicle; machine (Kangxi radical 159)\n(written) chariot",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"車",
+    "pinyin":"chē, jū",
+    "english":"car; vehicle; machine (Kangxi radical 159)\n(written) chariot",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"哪",
+    "pinyin":"nǎ, něi, na",
+    "english":"which; how\n(used for 啊 after words ending with -n)"
+  },
+  {
+    "char":"去",
+    "pinyin":"qù",
+    "english":"go; to leave"
+  },
+  {
+    "char":"太",
+    "pinyin":"tài",
+    "english":"too (much); excessively; highest; greatest"
+  },
+  {
+    "char":"百",
+    "pinyin":"bǎi",
+    "english":"hundred"
+  },
+  {
+    "char":"怎",
+    "pinyin":"zěn",
+    "english":"how; what; why"
+  },
+  {
+    "char":"什",
+    "pinyin":"shén, shí",
+    "english":"what\nassorted; ten"
+  },
+  {
+    "char":"么",
+    "pinyin":"me, ma",
+    "english":"interrogative/indefinite final particle",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"麼",
+    "pinyin":"me",
+    "english":"interrogative/indefinite final particle",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"人",
+    "pinyin":"rén",
+    "english":"person; man; people (Kangxi radical 9)"
+  },
+  {
+    "char":"了",
+    "pinyin":"le, liǎo, liào",
+    "english":"(particle for a new situation or completed action)\nfinish; understand; clear\nobserve",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"了",
+    "pinyin":"le, liǎo, liào",
+    "english":"(particle for a new situation or completed action), finish; understand; clear",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"回",
+    "pinyin":"huí",
+    "english":"to return; to reply; to go back; (mw for matters or actions)"
+  },
+  {
+    "char":"来",
+    "pinyin":"lái",
+    "english":"come; arrive; ever since; next",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"來",
+    "pinyin":"lái",
+    "english":"come; arrive; ever since; next",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"他",
+    "pinyin":"tā",
+    "english":"he; him"
+  },
+  {
+    "char":"就",
+    "pinyin":"jiù",
+    "english":"then; at once; just; only; with regard to"
+  },
+  {
+    "char":"还",
+    "pinyin":"hái, huán",
+    "english":"still; yet; in addition; even\nrepay; to return",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"還",
+    "pinyin":"hái, huán",
+    "english":"still; yet; in addition; even\nrepay; to return",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"也",
+    "pinyin":"yě",
+    "english":"also; too"
+  },
+  {
+    "char":"看",
+    "pinyin":"kàn, kān",
+    "english":"see; look at; to watch; regard\nwatch over; look after"
+  },
+  {
+    "char":"大",
+    "pinyin":"dà, dài",
+    "english":"big; large (Kangxi radical 37)\nperson of importance"
+  },
+  {
+    "char":"买",
+    "pinyin":"mǎi",
+    "english":"to buy",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"買",
+    "pinyin":"mǎi",
+    "english":"to buy",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"得",
+    "pinyin":"dé, děi, de",
+    "english":"obtain\nmust\n(complement particle)"
+  },
+  {
+    "char":"小",
+    "pinyin":"xiǎo",
+    "english":"small; young (Kangxi radical 42)"
+  },
+  {
+    "char":"书",
+    "pinyin":"shū",
+    "english":"book; letter",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"書",
+    "pinyin":"shū",
+    "english":"book; letter",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"再",
+    "pinyin":"zài",
+    "english":"again; once more"
+  },
+  {
+    "char":"都",
+    "pinyin":"dōu, dū",
+    "english":"all; both (if two things are involved)\ncapital; metropolis"
+  },
+  {
+    "char":"说",
+    "pinyin":"shuō",
+    "english":"speak; to say; to talk",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"說",
+    "pinyin":"shuō",
+    "english":"speak; to say; to talk",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"子",
+    "pinyin":"zǐ, zi",
+    "english":"son; child; (1st Earthly Branch) (Kangxi radical 39); seed\n(noun suffix)"
+  },
+  {
+    "char":"点",
+    "pinyin":"diǎn",
+    "english":"a dot; a point; a bit; a little; o'clock",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"點",
+    "pinyin":"diǎn",
+    "english":"a dot; a point; a bit; a little; o'clock",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"真",
+    "pinyin":"zhēn",
+    "english":"real; true; genuine"
+  },
+  {
+    "char":"想",
+    "pinyin":"xiǎng",
+    "english":"think; believe; suppose; would like to; remember with longing; to miss"
+  },
+  {
+    "char":"跟",
+    "pinyin":"gēn",
+    "english":"to follow; go with; heel"
+  },
+  {
+    "char":"对",
+    "pinyin":"duì",
+    "english":"correct; a pair; to face; be opposite; to; towards",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"對",
+    "pinyin":"duì",
+    "english":"correct; a pair; to face; be opposite; to; towards",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"水",
+    "pinyin":"shuǐ",
+    "english":"water (Kangxi radical 85)"
+  },
+  {
+    "char":"先",
+    "pinyin":"xiān",
+    "english":"first; early; before"
+  },
+  {
+    "char":"次",
+    "pinyin":"cì",
+    "english":"(mw for number of times of occurrence); nth; order"
+  },
+  {
+    "char":"打",
+    "pinyin":"dǎ, dá",
+    "english":"to hit; to beat; to play; to type\ndozen"
+  },
+  {
+    "char":"工",
+    "pinyin":"gōng",
+    "english":"work; skill (Kangxi radical 48)"
+  },
+  {
+    "char":"过",
+    "pinyin":"guò",
+    "english":"to pass; to cross; go over; (indicates a past experience)",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"過",
+    "pinyin":"guò",
+    "english":"to pass; to cross; go over; (indicates a past experience)",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"坐",
+    "pinyin":"zuò",
+    "english":"sit; take a seat"
+  },
+  {
+    "char":"茶",
+    "pinyin":"chá",
+    "english":"tea"
+  },
+  {
+    "char":"会",
+    "pinyin":"huì, kuài",
+    "english":"know how; be good at; be likely to; meeting; conference\naccounts",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"會",
+    "pinyin":"huì, kuài",
+    "english":"know how; be good at; be likely to; meeting; conference\naccounts",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"做",
+    "pinyin":"zuò",
+    "english":"do; make; to produce; be; to cook; write"
+  },
+  {
+    "char":"文",
+    "pinyin":"wén",
+    "english":"language; literature; culture; writing (Kangxi radical 67)"
+  },
+  {
+    "char":"学",
+    "pinyin":"xué",
+    "english":"to study; imitate; learning; science; school",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"學",
+    "pinyin":"xué",
+    "english":"to study; imitate; learning; science; school",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"两",
+    "pinyin":"liǎng",
+    "english":"two; 2; both; (unit of weight equal to 50 grams)",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"兩",
+    "pinyin":"liǎng",
+    "english":"two; 2; both; (unit of weight equal to 50 grams)",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"每",
+    "pinyin":"měi",
+    "english":"each; every"
+  },
+  {
+    "char":"生",
+    "pinyin":"shēng",
+    "english":"be born; give birth; life; raw (Kangxi radical 100)"
+  },
+  {
+    "char":"个",
+    "pinyin":"ge, gè",
+    "english":"general measure word\nindividual",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"個",
+    "pinyin":"ge, gè",
+    "english":"general measure word; individual",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"多",
+    "pinyin":"duō",
+    "english":"many; much; a lot of; more than"
+  },
+  {
+    "char":"少",
+    "pinyin":"shǎo, shào",
+    "english":"few; little; lack; lose\nyoung"
+  },
+  {
+    "char":"时",
+    "pinyin":"shí",
+    "english":"time; o'clock; when",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"時",
+    "pinyin":"shí",
+    "english":"time; o'clock; when",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"候",
+    "pinyin":"hòu",
+    "english":"await; inquire after"
+  },
+  {
+    "char":"吃",
+    "pinyin":"chī",
+    "english":"eat"
+  },
+  {
+    "char":"台",
+    "pinyin":"tái",
+    "english":"platform; Taiwan (abbr.); desk; stage; typhoon; (mw for machines); (classical) you (in letters)",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"台",
+    "pinyin":"tái",
+    "english":"Taiwan (abbr.); (classical) you (in letters)",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"样",
+    "pinyin":"yàng",
+    "english":"manner; pattern; way; type; kind",
+    "tradsimp":"[simplified]"
+  },
+  {
+    "char":"樣",
+    "pinyin":"yàng",
+    "english":"manner; pattern; way; type; kind",
+    "tradsimp":"[traditional]"
+  },
+  {
+    "char":"最",
+    "pinyin":"zuì",
+    "english":"the most; -est; (indicator for superlative)"
+  },
+  {
+    "char":"家",
+    "pinyin":"jiā",
+    "english":"family; home; household"
+  },
+  {
+    "char":"前",
+    "pinyin":"qián",
+    "english":"before; in front; forward"
+  },
+  {
+    "char":"",
+    "pinyin":"",
+    "english":""
+  },
+  {
+    "char":"",
+    "pinyin":"",
+    "english":""
+  },
+  {
+    "char":"",
+    "pinyin":"",
+    "english":""
+  },
+  {
+    "char":"",
+    "pinyin":"",
+    "english":""
+  },
+  {
+    "char":"",
+    "pinyin":"",
+    "english":""
+  },
+  {
+    "char":"",
+    "pinyin":"",
+    "english":""
+  }
+];
